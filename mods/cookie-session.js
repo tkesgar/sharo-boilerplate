ca60eb038e489591b160b91c5a0a04f2f8af198e
Ted Kesgar <t.kesgar@live.com>
2018-09-15 09:06:33 +0700

    SESSION_KEYS is now required for cookie-session mod

--- mods/cookie-session.js
+++ mods/cookie-session.js
@@ -1,15 +1,12 @@
-const crypto = require('crypto')
 const cookieSession = require('cookie-session')
 const {getProduction} = require('../lib/env')
 
 /**
- * This mod adds cookie-based session to app.
+ * This mod adds cookie-based session provider middleware to app.
  *
- * Note that `SESSION_KEYS` environment variable **should** be provided.
- * If `SESSION_KEYS` is not provided, a random string will be generated;
- * however, all client session cookies will be invalidated when the app
- * restarts. To provide multiple secret in `SESSION_KEYS`, split it with
- * semicolon (`;`) e.g. `'abc;123;456'`.
+ * Note that `SESSION_KEYS` environment variable is required. If it is not
+ * provided, this mod will throw an error. To provide multiple secret in
+ * `SESSION_KEYS`, split it with semicolon (`;`) e.g. `'abc;123;456'`.
  *
  * `secure` is enabled if `trust proxy` app settings table is set to truthy.
  * Otherwise, `secure` is enabled only in production.
@@ -19,23 +16,15 @@ const {getProduction} = require('../lib/env')
  * @param {Express.Application} app Express app to be modded
  */
 function modCookieSession(app) {
+  const {SESSION_KEYS: sessionKeys} = process.env
+  if (!sessionKeys) {
+    throw new Error('\'SESSION_KEYS\' environment variable does not exist')
+  }
+
   app.use(cookieSession({
-    keys: (process.env.SESSION_KEYS || createRandomString(18)).split(';'),
+    keys: sessionKeys.split(';'),
     secure: Boolean(app.get('trust proxy')) || getProduction()
   }))
 }
 
 module.exports = modCookieSession
-
-/**
- * Helper function to generate a random string with `length` characters.
- *
- * Note that the length of generated string is always even and less than
- * or equal to `length`.
- *
- * @param {number} length Length of the string to be created
- * @returns {string} A random hex string as described
- */
-function createRandomString(length) {
-  return crypto.randomBytes(length / 2).toString('hex')
-}
