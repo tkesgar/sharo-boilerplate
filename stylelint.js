aea21e99c2aebb3598ac79954c09176cb188a941
Ted Kesgar <t.kesgar@live.com>
2018-10-25 21:11:28 +0700

    Add stylelint shareable config, add deps in package.json

---
+++ stylelint.js
@@ -0,0 +1,12 @@
+/**
+ * A stylelint configuration usable in repository.
+ */
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss'
+  ],
+  plugins: [
+    'stylelint-scss'
+  ]
+}
