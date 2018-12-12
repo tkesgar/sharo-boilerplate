a971abbd23c3ea407a71270489e244d9395ceaf7
Ted Kesgar <t.kesgar@live.com>
2018-12-12 11:35:54 +0700

    Add babel config

---
+++ babel.js
@@ -0,0 +1,14 @@
+module.exports = () => ({
+  presets: [
+    [
+      'next/babel',
+      {
+        'styled-jsx': {
+          plugins: [
+            'styled-jsx-plugin-postcss'
+          ]
+        }
+      }
+    ]
+  ]
+})
