4c71ef39e950dfd7d8d321f3c72cbcfc7e89d7cd
Ted Kesgar <t.kesgar@live.com>
2019-02-25 09:48:53 +0700

    Cleanup sharo-scripts/xo

--- xo.js
+++ xo.js
@@ -1,15 +1,24 @@
-/* eslint-disable quote-props */
-
 module.exports = {
-  extends: 'xo-react',
+  // ESLint configurations
+  // =====================
+  extends: [
+    'xo-react'
+  ],
+  // Use babel-eslint parser for esnext features.
   parser: 'babel-eslint',
-  space: 2,
-  semicolon: false,
   settings: {
     'import/resolver': {
       node: {
+        // Add .md, .mdx, and .scss into autoresolved extensions
         extensions: ['.js', '.jsx', '.md', '.mdx', '.scss']
       }
     }
+  },
+  // XO configurations
+  // =================
+  // Note: wait for https://github.com/xojs/xo/issues/352
+  xo: {
+    space: 2,
+    semicolon: false
   }
 }
